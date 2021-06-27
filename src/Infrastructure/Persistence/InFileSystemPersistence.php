8950ea6d83fd5dee62f03832a11494c76c96c7c1
Daniel Sanz <langares@yahoo.es>
2021-06-27 21:52:59 +0200

    Update InFileSystemPersistence.php
    Pecado capital mezclar los datos con la lógica.
    
    Quizá la clase InFileSystemPersistence solo sea un contenedor de datos necesario para poder realizar el ejercicio pero esta separación me parece muy importante.
    
    De tal forma elijo desacoplar la información del código.

--- src/Infrastructure/Persistence/InFileSystemPersistence.php
+++ src/Infrastructure/Persistence/InFileSystemPersistence.php
@@ -4,32 +4,40 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Persistence;
 
-use App\Domain\Ad;
-use App\Domain\Picture;
 
 final class InFileSystemPersistence
 {
-    private array $ads = [];
-    private array $pictures = [];
-
-    public function __construct()
+    const ADS = [
+        [ 1, 'CHALET', 'Este piso es una ganga, compra, compra, COMPRA!!!!!', [], 300, null, null, null ],
+        [ 2, 'FLAT', 'Nuevo ático céntrico recién reformado. No deje pasar la oportunidad y adquiera este ático de lujo', [4], 300, null, null, null ],
+        [ 3, 'CHALET', '', [2], 300, null, null, null ],
+        [ 4, 'FLAT', 'Ático céntrico muy luminoso y recién reformado, parece nuevo', [5], 300, null, null, null ],
+        [ 5, 'FLAT', 'Pisazo,', [3, 8], 300, null, null, null ],
+        [ 6, 'GARAGE', '', [6], 300, null, null, null ],
+        [ 7, 'GARAGE', 'Garaje en el centro de Albacete', [], 300, null, null, null ],
+        [ 8, 'CHALET', 'Maravilloso chalet situado en lAs afueras de un pequeño pueblo rural. El entorno es espectacular, las vistas magníficas. ¡Cómprelo ahora!', [1, 7], 300, null, null, null ],
+    ];
+        
+    const PICTURES = [
+        [ 1, 'https://www.idealista.com/pictures/1', 'SD' ],
+        [ 2, 'https://www.idealista.com/pictures/2', 'HD' ],
+        [ 3, 'https://www.idealista.com/pictures/3', 'SD' ],
+        [ 4, 'https://www.idealista.com/pictures/4', 'HD' ],
+        [ 5, 'https://www.idealista.com/pictures/5', 'SD' ],
+        [ 6, 'https://www.idealista.com/pictures/6', 'SD' ],
+        [ 7, 'https://www.idealista.com/pictures/7', 'SD' ],
+        [ 8, 'https://www.idealista.com/pictures/8', 'HD' ],
+    ];
+    
+    
+    public function getAds ( )
     {
-        array_push($this->ads, new Ad(1, 'CHALET', 'Este piso es una ganga, compra, compra, COMPRA!!!!!', [], 300, null, null, null));
-        array_push($this->ads, new Ad(2, 'FLAT', 'Nuevo ático céntrico recién reformado. No deje pasar la oportunidad y adquiera este ático de lujo', [4], 300, null, null, null));
-        array_push($this->ads, new Ad(3, 'CHALET', '', [2], 300, null, null, null));
-        array_push($this->ads, new Ad(4, 'FLAT', 'Ático céntrico muy luminoso y recién reformado, parece nuevo', [5], 300, null, null, null));
-        array_push($this->ads, new Ad(5, 'FLAT', 'Pisazo,', [3, 8], 300, null, null, null));
-        array_push($this->ads, new Ad(6, 'GARAGE', '', [6], 300, null, null, null));
-        array_push($this->ads, new Ad(7, 'GARAGE', 'Garaje en el centro de Albacete', [], 300, null, null, null));
-        array_push($this->ads, new Ad(8, 'CHALET', 'Maravilloso chalet situado en lAs afueras de un pequeño pueblo rural. El entorno es espectacular, las vistas magníficas. ¡Cómprelo ahora!', [1, 7], 300, null, null, null));
-
-        array_push($this->pictures, new Picture(1, 'https://www.idealista.com/pictures/1', 'SD'));
-        array_push($this->pictures, new Picture(2, 'https://www.idealista.com/pictures/2', 'HD'));
-        array_push($this->pictures, new Picture(3, 'https://www.idealista.com/pictures/3', 'SD'));
-        array_push($this->pictures, new Picture(4, 'https://www.idealista.com/pictures/4', 'HD'));
-        array_push($this->pictures, new Picture(5, 'https://www.idealista.com/pictures/5', 'SD'));
-        array_push($this->pictures, new Picture(6, 'https://www.idealista.com/pictures/6', 'SD'));
-        array_push($this->pictures, new Picture(7, 'https://www.idealista.com/pictures/7', 'SD'));
-        array_push($this->pictures, new Picture(8, 'https://www.idealista.com/pictures/8', 'HD'));
+        return self::ADS;
+    }
+    
+    
+    public function getPictures ( )
+    {
+        return self::PICTURES;
     }
 }
