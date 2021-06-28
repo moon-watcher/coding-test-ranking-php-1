c45afd76e1a2af9c7fde6074290e745a17b40cfa
Daniel Sanz <langares@yahoo.es>
2021-06-28 12:36:44 +0200

    Update Controllers

--- src/Controller/Quality/ListsController.php
+++ src/Controller/Quality/ListsController.php
@@ -6,21 +6,136 @@ namespace App\Controller\Quality;
 use App\Classes\Base\ListsBaseController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Domain\Ad;
+use App\Classes\QualityQuantifier;
+use App\Infrastructure\Persistence\InFileSystemPersistence;
 
 
 class ListsController extends ListsBaseController
 {
+    private $minQuality;
+    
+    
+    public function __construct(  )
+    {
+        //
+        // En otras parte he optado por usar constantes para almacenar datos.
+        //
+        // En este caso he preferido utilizar config/sercices.yml. Para ello uso
+        // la clase AbstractController de Symfony extendiendo ListsBaseController,
+        // de la que a su vez, extiende la clase actual.
+        // 
+
+        $this->minQuality = 33; //$this->getParameter('quantify.min');
+    }
+
+
+    //
+    // case 2: listar los anuncios para un usuario de idealista
+    // 
+    // 1. Debe estar ordenado de mayor a menor score
+    // 2. No mostrar anuncios con score <40
+    //
     public function listAdByUserV1 ( Request $request )
     {
-        $userId = $request->get("userId");
-        dd($userId);
+        // Opcional, no se usa
+        $userId = $request->get("userId");        
         
-        return new Response ( "Woohoo, el endpoint del user " );
+        
+        $ads = $this->quantify_case2();
+        dd ( $ads );
+        
+        // Nunca llegará aquí.
+        return new Response ( "Woohoo, el endpoint del user" );
     }
     
-
+    
+    
+    //
+    // case 3: listar los anuncios para el responsable
+    // 
+    // 1. Lista anuncios para el resposable
+    // 2. Muestra los anuncios irrelevantes
+    // 3. Muestra la fecha desde cuando son irrelevantes
+    //
     public function listAdsForManagerV1()
     {
+        $ads = $this->quantify_case3();
+        dd ( $ads );
+        
+        // Nunca llegará aquí.
         return new Response ( "Hey! soy el endpoint del Manager" );
+    }
+    
+    
+    
+
+    
+    private function quantify_case2 ( ): array
+    {
+        $ads     = [ ];
+        $return  = [ ];
+        $adsData = InFileSystemPersistence::getAds ( );
+        $min     = $this->getParameter('quantify.min');
+
+        foreach ( $adsData as $key => $adData )
+        {
+            $ad = new Ad ( $key, $adData[0], $adData[1], $adData[2], $adData[3], $adData[4], $adData[5], $adData[6] );
+            $ad->quantify ( QualityQuantifier::class, $this->minQuality );
+            
+            $score = $ad->getFinalScore();
+            
+            if ( $score >= $min )
+            {
+                //
+                // Usar $score como índice. 
+                // 
+                // Como puede haber anuncos con igual score 
+                // el subarray ayudará a evitar colisiones.
+                //
+                $ads [ $score ] [ ] = $ad; 
+            }
+        }
+        
+        
+        //
+        // Ordenación easy peasy.
+        // Ahora hay que desacoplar el subarray.
+        // 
+        // De un foreach a otro no he cambiado mis opiniones 
+        // sobre los array_*($callback)
+        //
+        foreach ( $ads as $array )
+        {
+            foreach ( $array as $ad )
+            {
+               $return [] = $ad;
+            }
+        }
+                
+        return $return;
+    }
+
+    
+    private function quantify_case3 ( ): array
+    {
+        $ads     = [ ];
+        $adsData = InFileSystemPersistence::getAds ( );
+        $min     = $this->getParameter('quantify.min');
+   
+        foreach ( $adsData as $key => $adData )
+        {
+            $ad    = new Ad ( $key, $adData[0], $adData[1], $adData[2], $adData[3], $adData[4], $adData[5], $adData[6] );
+            $ad->quantify ( QualityQuantifier::class, $this->minQuality );
+            
+            $score = $ad->getFinalScore();
+
+            if ( $score < $min )
+            {
+                $ads [ ] = $ad; 
+            }
+        }
+        
+        return $ads;
     }
 }
--- src/Controller/Quality/ScoresController.php
+++ src/Controller/Quality/ScoresController.php
@@ -3,14 +3,51 @@
 namespace App\Controller\Quality;
 
 
+use App\Domain\Ad;
 use App\Classes\Base\ScoresBaseController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Infrastructure\Persistence\InFileSystemPersistence;
+use App\Classes\QualityQuantifier;
 
 
 class ScoresController extends ScoresBaseController
 {
+    private $minQuality;
+    
+    //
+    // case 1: para calcular la puntuación de todos los anuncios
+    //
     public function showScoresV1 ( )
-    {        
-        return new Response ( "hola, soy el tío Scores :)" );
+    {
+        $quantities = [];
+        $adsData    = InFileSystemPersistence::getAds();
+        $min        = $this->getParameter('quantify.min');
+       
+        foreach ( $adsData as $key => $adData )
+        {
+            $quantities [ ] = $this->quantify ( $key, $adData, $min );
+        }
+        
+        
+        //
+        // Muestra anuncios con los scores ya calculados.
+        //
+        // Quizá usar dd() no sea lo más óptimo pero cumple 
+        // con el objetivo de la prueba.
+        //
+        dd ( $quantities );
+        
+        
+        // Nunca llegará aquí.
+        return new Response ( $quantities );
+    }
+    
+    
+    private function quantify ( $key, $array, $min ): Ad
+    {
+        $ad = new Ad ( $key, $array[0], $array[1], $array[2], $array[3], $array[4], $array[5], $array[6] );
+        $ad->quantify ( QualityQuantifier::class, $min );
+                
+        return $ad;
     }
 }
