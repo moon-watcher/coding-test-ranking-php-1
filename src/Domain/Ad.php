701439cd97daf402e6bae74b106fc370e35e5941
Daniel Sanz <langares@yahoo.es>
2021-06-28 02:55:57 +0200

    Update Ad.php
    Implementadas las funciones necesarias para obtener y preservas el $score del anuncio.

--- src/Domain/Ad.php
+++ src/Domain/Ad.php
@@ -26,24 +26,60 @@ final class Ad
     }
     
     
-    public function quantify ( )
+    public function quantify ( ) :int
     {
         return QualityQuantifier::Quantify ( $this );
     }
     
     
     //
-    // Aunque esto no sea buena idea porque ignora los private 
-    // me gusta dejarlo aquí para demostrar que se puede usar
+    // Aunque esto no sea buena práctica porque ignora las declaraciones 
+    // private de __contruct() me gusta dejarlo aquí para demostrar que 
+    // se puede usar.
+    // 
+    // Esto me evita tener que cear todos los getters.
+    // 
+    // Por otro lado ¿no pensáis que se abusa demasiado de los getters 
+    // y los setters?
     //
     function __get ( $name )
     {
         return $this->$name;
     }
     
-    
+        
+    //
+    // Creado por cortesía profesional
+    //
     function getTypology ( )
     {
         return $this->typology;
+    }
+    
+    
+    function setScore ( ?int $score )
+    {
+        $this->score = $score;
+    }
+    
+    
+    function incScore ( int $score ): int
+    {
+        $this->setScore ( $this->score + $score );
+        
+        return $this->score;
+    }
+    
+    
+    function getFinalScore ( ): int
+    {
+        $score = $this->score;
+        
+        if ( $score < 0 )
+        {
+            $score = 0;
+        }
+        
+        return $score;
     }
 }
